aef3701a0808aaa25123d562dfd6969620bbcacd
Matt Schleder <schlederboss@gmail.com>
2023-12-14 10:49:23 -0500

    init commit with export_metadata.py

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # spacestation-tautulli-utilities
+
 Python scripts to access information for SpaceStation's tautulli instance
+
+## Setup
+
+This assumes that you have a running Tautulli instance and is already configured to your plex server. These only need to be done once
+
+1. Download Python, if you don't already have it
+1. Navigate to the .env.template and replace the two variables with your instance and API key, do not add an ending '/' to the root URL
+1. Rename `.env.template` to `.env`
+1. Open a terminal window and run `pip install -r requirements.txt`
+
+## Running the scripts
+
+1. From any terminal window, run
+
+```shell
+python scripts/file_name.py
+```
+
+You will be greeted with messages for output files or errors, if any occur
---
+++ scripts/export_metadata.py
@@ -0,0 +1,50 @@
+#!/bin/python
+
+# This will export section metadata to determine what has and hasn't been watched
+# It will create a file based on the field
+import requests
+import os
+import csv
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+root_url = os.getenv("TAUTULLI_ROOT_URL")
+
+if not (api_key or root_url):
+    print("Please set variables for API_KEY and TAUTULLI_ROOT_URL in .env")
+    exit()
+
+
+libraries = requests.get(f"{root_url}/api/v2?apikey={api_key}&cmd=get_libraries")
+lib_json = libraries.json()
+
+for item in lib_json["response"]["data"]:
+    media_info = requests.get(
+        f"{root_url}/api/v2?apikey={api_key}&cmd=get_library_media_info&section_id={item['section_id']}&length=5000"
+    )
+    movie_info = media_info.json()
+    with open(f"{item['section_name']}.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        field = ["title", "rating_key (id)", "file_size", "play_count", "last_played"]
+
+        writer.writerow(field)
+        for movie_info in movie_info["response"]["data"]["data"]:
+            play_count = 0
+            last_played = 0
+            if movie_info["play_count"]:
+                play_count = movie_info["play_count"]
+            if movie_info["last_played"]:
+                last_played = movie_info["last_played"]
+            writer.writerow(
+                [
+                    movie_info["title"],
+                    movie_info["rating_key"],
+                    movie_info["file_size"],
+                    play_count,
+                    last_played,
+                ]
+            )
+        print(f"Successfully written to {item['section_name']}.csv")
